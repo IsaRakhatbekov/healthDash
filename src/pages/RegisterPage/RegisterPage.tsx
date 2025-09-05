8ff9e9f500a16576d5f6bc1660b4b4d42df50075
Isa <isarahatbekov250@gmail.com>
2025-09-05 20:05:38 +0600

    restart registerPage and loginPage

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,49 +1,20 @@
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { registerSchema } from "./validation";
 import styles from "./RegisterPage.module.scss";
 
-type RoleProps = {
-  role: string;
-};
-
-const RegisterPage: React.FC<RoleProps> = ({ role }) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(registerSchema), // схема yup
-  });
-
-  const onSubmit = (data: any) => {
-    console.log("Регистрация с данными:", data);
-  };
-
+const RegisterPage = () => {
   return (
     <div className={styles.register}>
       <div className={styles.inner}>
         <h1 className={styles.title}>Create your account</h1>
         <p className={styles.subTitle}>
-          Role selected: <span>{role}</span>
+          Role selected: <span></span>
         </p>
 
-        <form className={styles.regisForm} onSubmit={handleSubmit(onSubmit)}>
+        <form className={styles.regisForm}>
           <div className={styles.inputWrapper}>
             <label className={styles.label} htmlFor="email">
               Email <span>We’ll send a verification code to this email*</span>
             </label>
-            <input
-              id="email"
-              type="email"
-              {...register("email")}
-              className={`${styles.input} ${
-                errors.email ? styles.errorInput : ""
-              }`}
-            />
-            {errors.email && (
-              <p className={styles.errorText}>{errors.email.message}</p>
-            )}
+            <input id="" type="" />
           </div>
 
           <div className={styles.inputWrapper}>
@@ -51,41 +22,15 @@ const RegisterPage: React.FC<RoleProps> = ({ role }) => {
               Password
               <span>8+ characters, at least 1 letter and 1 number*</span>
             </label>
-            <input
-              id="password"
-              type="password"
-              {...register("password")}
-              className={`${styles.input} ${
-                errors.password ? styles.errorInput : ""
-              }`}
-            />
-            {errors.password && (
-              <p className={styles.errorText}>{errors.password.message}</p>
-            )}
+            <input id="password" type="password" />
           </div>
 
           <div className={styles.inputWrapper}>
             <label className={styles.label} htmlFor="repeatPassword">
               Repeat Password
             </label>
-            <input
-              id="repeatPassword"
-              type="password"
-              {...register("repeatPassword")}
-              className={`${styles.input} ${
-                errors.repeatPassword ? styles.errorInput : ""
-              }`}
-            />
-            {errors.repeatPassword && (
-              <p className={styles.errorText}>
-                {errors.repeatPassword.message}
-              </p>
-            )}
+            <input id="repeatPassword" type="password" />
           </div>
-
-          <button className={styles.guestBtn} type="submit">
-            Continue
-          </button>
         </form>
       </div>
     </div>
