249af2a2282ff64a705e3bc2ab0091f8fc986d27
Isa <isarahatbekov250@gmail.com>
2025-09-21 22:54:13 +0600

    finished ChatSidebar

--- src/pages/PatientPage/Chat/Chat.tsx
+++ src/pages/PatientPage/Chat/Chat.tsx
@@ -1,10 +1,12 @@
 import styles from "./Chat.module.scss";
 import ChatSidebar from "./ChatSidebar/ChatSidebar";
+import ChatWindow from "./ChatWindow/ChatWindow";
 
 const Chat = () => {
   return (
     <div className={styles.chat}>
       <ChatSidebar />
+      <ChatWindow />
     </div>
   );
 };
---
+++ src/pages/PatientPage/Chat/ChatWindow/ChatWindow.tsx
@@ -0,0 +1,12 @@
+import styles from "./ChatWindow.module.scss";
+
+const ChatWindow = () => {
+  return (
+    <div className={styles.chatWindow}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque rem ipsam
+      doloribus sit, molestias molestiae quae alias ea quas quo!
+    </div>
+  );
+};
+
+export default ChatWindow;
